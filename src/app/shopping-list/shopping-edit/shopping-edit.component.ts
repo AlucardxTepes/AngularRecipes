13f2640025e1b34e1db2f9e3341473d0c25e5d47
AlucardxTepes <alucard.557@gmail.com>
2017-09-06 23:13:14 -0400

    Add shopping list service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,21 +1,18 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @Output() addIngredientEmitter = new EventEmitter<Ingredient>();
+export class ShoppingEditComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   onAddClicked(name: string, amount: string) {
-    this.addIngredientEmitter.emit(new Ingredient(name, Number.parseInt(amount)));
+    this.shoppingListService.create(new Ingredient(name, Number.parseInt(amount)));
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  onIngredientListUpdated = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Onions', 3)
+  ];
+
+  create(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.onIngredientListUpdated.emit(this.ingredients.slice());
+  }
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
 }
