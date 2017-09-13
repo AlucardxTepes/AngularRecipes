037d72938ce7121e77beb4fde6c3818165d358d5
AlucardxTepes <alucard.557@gmail.com>
2017-09-13 19:48:21 -0400

    Replace http with newer HttpClient and add http interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,6 +7,9 @@ import {AuthService} from '../auth/auth.service';
 import {DataStorageService} from '../shared/data-storage.service';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 import {RecipeService} from '../recipes/recipe.service';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthInterceptor} from '../shared/auth.interceptor';
+import {LoggingInterceptor} from '../shared/logging.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,14 @@ import {RecipeService} from '../recipes/recipe.service';
     AppRoutingModule,
     HeaderComponent
   ],
-  providers: [RecipeService, ShoppingListService, DataStorageService, AuthService]
+  providers: [
+    RecipeService,
+    ShoppingListService,
+    DataStorageService,
+    AuthService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true}
+  ]
 })
-export class CoreModule {}
+export class CoreModule {
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,25 +1,32 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import 'rxjs/add/operator/map';
 import {Recipe} from '../recipes/recipe.model';
 import {AuthService} from '../auth/auth.service';
+import {HttpClient, HttpParams, HttpRequest} from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {
+  }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-    return this.http.put('https://angular-http-test-60147.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    // return this.http.put('https://angular-http-test-60147.firebaseio.com/recipes.json', this.recipeService.getRecipes(),
+    //   {
+    //     observe: 'body',
+    //     params: new HttpParams().set('auth', token)
+    //     // headers: headers
+    //   });
+    const req = new HttpRequest('PUT', 'https://angular-http-test-60147.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+      // {reportProgress: true, params: new HttpParams().set('auth', token)}
+    return this.http.request(req);
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-    return this.http.get('https://angular-http-test-60147.firebaseio.com/recipes.json?auth=' + token)
+    return this.http.get<Recipe[]>('https://angular-http-test-60147.firebaseio.com/recipes.json')
+      // {params: new HttpParams().set('auth', token)})
       .map(
-        (response: Response) => {
-          const recipes = response.json();
+        (recipes: Recipe[]) => {
           // avoid null recipes array
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
